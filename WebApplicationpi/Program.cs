664dadaeb4ab517bcb03e4a89e2ebcdffb86a82e
rasmus <rasmussonntag@gmail.com>
2022-02-03 17:39:49 +0100

    fix

--- WebApplicationpi/Program.cs
+++ WebApplicationpi/Program.cs
@@ -4,17 +4,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var hosts = new Dictionary<string,string>();
+var hosts = new Dictionary<string, string>();
 
 builder.Services.AddSingleton(hosts);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseHttpsRedirection();
